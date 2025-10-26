b1d7131b2f40aacac61dc40d6266d7ea3c3761ac
MALIK KAREEM <malikkareem852@gmail.com>
2025-10-26 18:37:23 +0700

    Minor Fix

--- burhan_shop/settings.py
+++ burhan_shop/settings.py
@@ -155,3 +155,6 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect unauthenticated users to our custom login page
+LOGIN_URL = 'main:login'
